80dfab2d5555d6b5910adfd59639aa241aee8447
Martin Tracey <matracey@outlook.ie>
2016-12-17 21:09:27 +0000

    Initial commit. End of chapter 3.

---
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ExploreCalifornia.Controllers
+{
+    [Route("blog")]
+    public class BlogController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Martin Tracey",
+                    Body = "This is a great blog post, don't you think?"
+                },
+                    new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Martin Tracey",
+                    Body = "This is a ANOTHER great blog post, don't you think?"
+                }
+            };
+
+            return View(posts);
+        }
+
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Martin Tracey",
+                Body = "This is a great blog post, don't you think?"
+            };
+
+            return View(post);
+        }
+    }
+}
